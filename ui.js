37d12d42b2642eaa45e5ca910e1cbc44d0ef96e2
Robin Hobbensiefken <hobben@gmail.com>
2017-03-19 07:30:30 -0700

    Updated to match the changed I had made to the FRC Dashboard project. Added batch file for launching.

--- ui.js
+++ ui.js
@@ -1,32 +1,31 @@
 // Define UI elements
 var ui = {
-    timer: document.getElementById('timer'),
-    robotState: document.getElementById('robot-state').firstChild,
-    gyro: {
-        container: document.getElementById('gyro'),
-        val: 0,
-        offset: 0,
-        visualVal: 0,
-        arm: document.getElementById('gyro-arm'),
-        number: document.getElementById('gyro-number')
-    },
-    robotDiagram: {
-        arm: document.getElementById('robot-arm')
-    },
-    example: {
-        button: document.getElementById('example-button'),
-        readout: document.getElementById('example-readout').firstChild
-    },
-    tuning: {
-        list: document.getElementById('tuning'),
-        button: document.getElementById('tuning-button'),
-        name: document.getElementById('name'),
-        value: document.getElementById('value'),
-        set: document.getElementById('set'),
-        get: document.getElementById('get')
-    },
-    autoSelect: document.getElementById('auto-select'),
-    armPosition: document.getElementById('arm-position')
+	timer: document.getElementById('timer'),
+	robotState: document.getElementById('robot-state'),
+	gyro: {
+		container: document.getElementById('gyro'),
+		val: 0,
+		offset: 0,
+		visualVal: 0,
+		arm: document.getElementById('gyro-arm'),
+		number: document.getElementById('gyro-number')
+	},
+	resetGyro: {
+		button: document.getElementById('gyro-reset'),
+	},
+	tuning: {
+		list: document.getElementById('tuning'),
+		button: document.getElementById('tuning-button'),
+		name: document.getElementById('name'),
+		value: document.getElementById('value'),
+		set: document.getElementById('set'),
+		get: document.getElementById('get')
+	},
+	autoSelect: document.getElementById('auto-select'),
+	controllerChooser: document.getElementById('controller-chooser'),
+	lidStatus: document.getElementById('lidStatus'),
+	driveMode: document.getElementById('driveMode'),
+	autoAim: document.getElementById('autoAim')
 };
 let address = document.getElementById('connect-address'), 
     connect = document.getElementById('connect');
@@ -80,7 +79,7 @@ function onRobotConnection(connected) {
             connect.disabled = false;
             connect.firstChild.data = "Connect";
             // Add the default address and select xxxx
-            address.value = "roborio-xxxx.local";
+            address.value = "10.9.57.2";
             address.focus();
             address.setSelectionRange(8, 12);
             // On click try to connect and disable the input and the button
@@ -99,39 +98,18 @@ function onRobotConnection(connected) {
 // Gyro rotation
 let updateGyro = (key, value) => {
     ui.gyro.val = value;
-    ui.gyro.visualVal = Math.floor(ui.gyro.val - ui.gyro.offset);
-    if (ui.gyro.visualVal < 0) {
-        ui.gyro.visualVal += 360;
-    }
-    ui.gyro.arm.style.transform = `rotate(${ui.gyro.visualVal}deg)`;
-    ui.gyro.number.innerHTML = ui.gyro.visualVal + 'º';
+    ui.gyro.arm.style.transform = ('rotate(' + ui.gyro.val + 'deg)');
+    ui.gyro.number.innerHTML = ui.gyro.val + 'º';
 };
-NetworkTables.addKeyListener('/SmartDashboard/drive/navx/yaw', updateGyro);
-
-// The following case is an example, for a robot with an arm at the front.
-// Info on the actual robot that this works with can be seen at thebluealliance.com/team/1418/2016.
-NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
-    // 0 is all the way back, 1200 is 45 degrees forward. We don't want it going past that.
-    if (value > 1140) {
-        value = 1140;
-    }
-    else if (value < 0) {
-        value = 0;
-    }
-    // Calculate visual rotation of arm
-    var armAngle = value * 3 / 20 - 45;
-    // Rotate the arm in diagram to match real arm
-    ui.robotDiagram.arm.style.transform = `rotate(${armAngle}deg)`;
-});
+NetworkTables.addKeyListener('/SmartDashboard/gyroValue', updateGyro);
 
 // This button is just an example of triggering an event on the robot by clicking a button.
-NetworkTables.addKeyListener('/SmartDashboard/example_variable', (key, value) => {
+NetworkTables.addKeyListener('/SmartDashboard/gyroReset', (key, value) => {
     // Sometimes, NetworkTables will pass booleans as strings. This corrects for that.
     if (typeof value === 'string')
         value = value === "true";
     // Set class active if value is true and unset it if it is false
     ui.example.button.classList.toggle('active', value);
-    ui.example.readout.data = 'Value is ' + (value ? 'true' : 'false');
 });
 
 NetworkTables.addKeyListener('/SmartDashboard/time_running', (key, value) => {
@@ -176,7 +154,7 @@ NetworkTables.addKeyListener('/SmartDashboard/time_running', (key, value) => {
 });
 
 // Load list of prewritten autonomous modes
-NetworkTables.addKeyListener('/SmartDashboard/time_running', (key, value) => {
+NetworkTables.addKeyListener('/SmartDashboard/autoChoices/options', (key, value) => {
     // Clear previous list
     while (ui.autoSelect.firstChild) {
         ui.autoSelect.removeChild(ui.autoSelect.firstChild);
@@ -191,8 +169,24 @@ NetworkTables.addKeyListener('/SmartDashboard/time_running', (key, value) => {
     ui.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
 });
 
+// Load list of prewritten controller options
+NetworkTables.addKeyListener('/SmartDashboard/controllerChooser/options', (key, value) => {
+    // Clear previous list
+    while (ui.autoSelect.firstChild) {
+        ui.autoSelect.removeChild(ui.autoSelect.firstChild);
+    }
+    // Make an option for each autonomous mode and put it in the selector
+    for (let i = 0; i < value.length; i++) {
+        var option = document.createElement('option');
+        option.appendChild(document.createTextNode(value[i]));
+        ui.autoSelect.appendChild(option);
+    }
+    // Set value to the already-selected mode. If there is none, nothing will happen.
+    ui.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedController');
+});
+
 // Load list of prewritten autonomous modes
-NetworkTables.addKeyListener('/SmartDashboard/autonomous/selected', (key, value) => {
+NetworkTables.addKeyListener('/SmartDashboard/autoChoices/selected', (key, value) => {
     ui.autoSelect.value = value;
 });
 
@@ -268,16 +262,16 @@ function onValueChanged(key, value, isNew) {
 
 }
 // The rest of the doc is listeners for UI elements being clicked on
-ui.example.button.onclick = function () {
+ui.resetGyro.button.onclick = function () {
     // Set NetworkTables values to the opposite of whether button has active class.
-    NetworkTables.putValue('/SmartDashboard/example_variable', this.className != 'active');
+	NetworkTables.setValue('/SmartDashboard/gyroReset', true);
 };
 // Reset gyro value to 0 on click
 ui.gyro.container.onclick = function () {
     // Store previous gyro val, will now be subtracted from val for callibration
     ui.gyro.offset = ui.gyro.val;
     // Trigger the gyro to recalculate value.
-    updateGyro('/SmartDashboard/drive/navx/yaw', ui.gyro.val);
+    updateGyro('/SmartDashboard/gyroValue', ui.gyro.val);
 };
 // Open tuning section when button is clicked
 ui.tuning.button.onclick = function () {
@@ -300,9 +294,9 @@ ui.tuning.get.onclick = function () {
 };
 // Update NetworkTables when autonomous selector is changed
 ui.autoSelect.onchange = function () {
-    NetworkTables.putValue('/SmartDashboard/autonomous/selected', this.value);
+    NetworkTables.putValue('/SmartDashboard/autoChoices/selected', this.value);
 };
 // Get value of arm height slider when it's adjusted
-ui.armPosition.oninput = function () {
-    NetworkTables.putValue('/SmartDashboard/arm/encoder', parseInt(this.value));
-};
+ui.controllerChooser.onchange = function() {
+	NetworkTables.setValue('/SmartDashboard/controllerChooser/selected', this.value);
+}
